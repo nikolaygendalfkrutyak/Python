f54c237372bb66f4b5d4b61521a448909054b8e4
nikolaygendalfkrutyak <nikolatarasenk@gmail.com>
2024-10-22 14:57:26 +0300

    parcing document task

--- hw_15-10/task5.py
+++ hw_15-10/task5.py
@@ -1,48 +1,66 @@
-def count_money(myMoney, is_final=True):
-    zero_nine= ["", "one", "two", "three", "four", "five","six","seven","eight","nine"]
-    tens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", 
-            "nineteen"]
-    twenty_ninty=["", "", "twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety"]
-    try:
-        myMoney=float(myMoney)
-        answer = ""
-        cents= int((myMoney-int(myMoney))*100)
-        myMoney=int(myMoney)
-        thousands = myMoney //1000
-        if thousands:
-            answer += f"{count_money(thousands,False)} thousand "
+# def count_money(myMoney, is_final=True):
+#     zero_nine= ["", "one", "two", "three", "four", "five","six","seven","eight","nine"]
+#     tens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", 
+#             "nineteen"]
+#     twenty_ninty=["", "", "twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety"]
+#     try:
+#         myMoney=float(myMoney)
+#         answer = ""
+#         cents= int((myMoney-int(myMoney))*100)
+#         myMoney=int(myMoney)
+#         thousands = myMoney //1000
+#         if thousands:
+#             answer += f"{count_money(thousands,False)} thousand "
             
-        myMoney%=1000
-        hundreds = myMoney //100
-        if hundreds:
-            answer+=f"{zero_nine[hundreds]} hundred "
-        myMoney%=100
-        ten = myMoney //10
-        myMoney%=10
-        if ten:
-            if ten!=1:            
-                answer+=f"{twenty_ninty[ten]} {zero_nine[myMoney]} "
-            else:
-                answer+=f"{tens[myMoney]}"
-        else:
-            answer+=f"{zero_nine[myMoney]}"
-        if not myMoney and not answer:
-            answer="Zero"
-        if (is_final):
-            answer+=" dollar"
-            if myMoney!=1 or ten==1:
-                answer+="s"
-            if cents:
-                answer+=f" and {count_money(cents, False)} cent"
-                if cents != 1:
-                    answer+="s"
-        else:
-            answer.lower()
-        return (answer)
+#         myMoney%=1000
+#         hundreds = myMoney //100
+#         if hundreds:
+#             answer+=f"{zero_nine[hundreds]} hundred "
+#         myMoney%=100
+#         ten = myMoney //10
+#         myMoney%=10
+#         if ten:
+#             if ten!=1:            
+#                 answer+=f"{twenty_ninty[ten]} {zero_nine[myMoney]} "
+#             else:
+#                 answer+=f"{tens[myMoney]}"
+#         else:
+#             answer+=f"{zero_nine[myMoney]}"
+#         if not myMoney and not answer:
+#             answer="Zero"
+#         if (is_final):
+#             answer+=" dollar"
+#             if myMoney!=1 or ten==1:
+#                 answer+="s"
+#             if cents:
+#                 answer+=f" and {count_money(cents, False)} cent"
+#                 if cents != 1:
+#                     answer+="s"
+#         else:
+#             answer.lower()
+#         return (answer)
         
-    except:
-        return("Wrong format")
+#     except:
+#         return("Wrong format")
     
 
-myMoney = input("Money: ")
-print(count_money(myMoney))
+# myMoney = input("Money: ")
+# print(count_money(myMoney))
+salary = {
+    'January': 1000,
+    'February': 1500,
+    'March': 2000,
+    'April': 2500,
+    'May': 3000,
+    'June': 3500,
+    'July': 4000,
+    'August': 4500,
+    'September': 5000,
+    'October': 5500,
+    'November': 6000,
+    'December': 6500
+}
+file = open('data.txt', 'w')
+for i in range(len(salary)):
+    file.write(f"{salary.keys[i]} : {salary.values[i]}$\n")
+file.close()
---
+++ hw_17-10/superTask.py
@@ -0,0 +1,61 @@
+def total_for_each(arr, cat):
+    """ Helps to find sum of money values for some categories
+
+    Args:
+        arr (Array): array of arrays of data. 
+            Each contains 3 positions [name_of_customer (String), category_of_shopping(String), money_spent(float)]  
+        cat (String): "name" or "category" 
+            Helps to understand by which of 2 categories (name_of_customer or category_of_shopping) should we divide our data
+
+    Returns:
+        dict : returns pairs (category_name : total_money_spent) 
+    """
+    if cat=="name":
+        my_index =0
+    elif cat=="category":
+        my_index=1
+    else:
+        return (total(arr))
+    res={}
+    for i in arr:
+        if i[my_index] in res:
+            res[i[my_index]] = round((res[i[my_index]]+i[2]), 2)
+        else:
+            res[i[my_index]]=i[2]
+    return res
+def total(arr):
+    """Sums all money spent aside of names and categories
+
+    Args:
+        arr (Array of Arrays): in each array there is 3 parameters, the last of them is money amount which we need
+    """
+    mySum=0
+    for i in arr:
+        mySum=round(mySum+i[-1], 2)
+    return(mySum)
+def parse_doc(link):
+    """parse data from a file with given path to a needed format
+
+    Args:
+        link (String): link to the file where data is stored
+
+    Returns:
+        array: returns sorted data after changning type if needed
+    """
+    try:
+        file = open(link, 'r')
+        res = []
+        for i in file.readlines():
+            i=i.split(" ")
+            name = " ".join(i[1:-2])
+            category =i[-1][:-1]
+            money = round(float(i[-2][:-1]), 2)
+            res.append([name, category, money])
+        file.close()
+        return res
+    except:
+        return None
+data = parse_doc("hw_10_test.txt")    
+print(f"Total sum is: {total(data)}")
+print(f"Each member spent: {total_for_each(data, "name")}")
+print(f"For each category were spent: {total_for_each(data, "category")}")
